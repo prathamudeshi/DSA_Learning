afe7972fccc6ab157f416c9070ad10fc599b336f
Pratham <udeshipratham@gmail.com>
2025-03-24 16:55:32 +0530

    24th March

---
+++ Stacks/Next_Smallest_element.cpp
@@ -0,0 +1,21 @@
+// https://www.naukri.com/code360/problems/next-smaller-element_1112581?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0%3Fsource%3Dyoutube&campaign=Lovebabbarcodestudio&leftPanelTabValue=SUBMISSION
+
+#include<bits/stdc++.h>
+
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> stk;
+    vector<int> ans(n);
+    stk.push(-1);
+    // ans[0] = stk.top();
+    // stk.push(arr[n-1]);
+    for(int i=n-1;i>=0;i--){
+        while(stk.top()>=arr[i]){
+            stk.pop();
+        }
+        ans[i] = stk.top();
+        stk.push(arr[i]);
+    }
+    return ans;
+    // Write your code here.
+}
